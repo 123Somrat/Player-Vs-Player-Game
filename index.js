82427081632b32372b4270d3d9db73718a4c9fca
Mohammad Jafar Uddin <mdjafaruddinsomrat@gmail.com>
2022-07-14 09:25:03 +0200

    Select all Ui element

---
+++ index.js
@@ -0,0 +1,9 @@
+// select UI element
+
+const p1ScoreElm = document.querySelector("#p1Score");
+const p2ScoreElm = document.querySelector("#p2Score");
+const inputScoreElm = document.querySelector("#inputScore");
+const playingToElm = document.querySelector(".playingTo");
+const p1BtnElm = document.querySelector("#p1Btn");
+const p2BtnElm = document.querySelector("#p2Btn");
+const resetBtnElm = document.querySelector("#resetBtn");
